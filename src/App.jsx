9a519565a7d3c7682d222e15f561050f394c2412
Jeffrey Gil P. Deimos <jeffroxgil@gmail.com>
2022-08-27 11:13:26 +0800

    Created Authentication.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import TodoList from "./components/TodoList";
+import Auth from "./components/authentication/Auth";
+import { useSelector } from "react-redux";
 
 function App() {
+  const activeUser = useSelector((state) => state.activeUser);
   return (
-    <div className="App">
-      <TodoList />
-    </div>
+    <div className="App">{activeUser.email ? <TodoList /> : <Auth />}</div>
   );
 }
 
